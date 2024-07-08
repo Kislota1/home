479272b38e36024c00c5543ea7aa84614269c56b
Oleg <larog02@mail.ru>
2024-07-08 21:42:27 +0300

    add upper func

--- str_func.py
+++ str_func.py
@@ -2,3 +2,12 @@ def to_uppercase():
     input_string = input("Введите текст: ")
     output_string = input_string.upper()
     return output_string
+
+def capitalize_words():
+    """
+    Меняет первые две буквы в слове на заглавные.
+    """
+    input_string = input("Введите текст: ")
+    words = input_string.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return ' '.join(capitalized_words)
